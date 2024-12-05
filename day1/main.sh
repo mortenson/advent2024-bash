a22ee0dfb32817151a9184ef1c5e0758a68854d2
Sam Mortenson <mortensonsam@gmail.com>
2024-12-05 08:22:42 -0800

    day1

---
+++ day1/main.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+LEFT=$(cat input.txt | awk '{ print $1 }' | sort)
+RIGHT=$(cat input.txt | awk '{ print $2 }' | sort)
+
+i=0
+TOTAL=0
+IFS=$'\n' RIGHTLINES=($RIGHT)
+echo "$LEFT" | while read p; do
+  DIFF=$((p - ${RIGHTLINES[$i]}))
+  DIFF=${DIFF#-}
+  ((TOTAL += DIFF))
+  ((++i))
+  echo $TOTAL
+done
